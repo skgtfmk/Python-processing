fbb8dc087dae2d3eac80788cd65a3141066fca0f
Fred Kuenzi <skgtfmk@ucl.ac.uk>
2026-08-26 18:01:14 +0100

    remove upload_date from CRO_CACO2_PERM

--- Py Luminescence Plate MAD.py
+++ Py Luminescence Plate MAD.py
@@ -1,4 +1,4 @@
-# Read blocks of data from an Excel file.
+# Read blocks of data from an Excel or csv file.
 # Each block starts at the line following a regular expression from the per-study
 # property "block identifier" and runs for for the full size of the plate.
 # The actual value is stored in the plate property "block identifier".
@@ -6,6 +6,7 @@ from com.dotmatics.dataig.studies import StudyUtils
 from java.io import File
 import re, os
 import csv
+from datetime import datetime
 
 def Calc_median(list_data):
     list_data.sort()
@@ -18,7 +19,7 @@ def txt_reader(file_path, dialect):
     return([line for line in reader])
 
 def read_data(data_path):
-    file_ext = os.path.splitext(data_path)[1]
+    file_ext = os.path.splitext(data_path)[1].lower()
     if file_ext in [".xlsx", ".xls"]:
         file_data = StudyUtils.convertExcelToArrayList(File(data_path))
     elif file_ext == ".csv":
@@ -28,6 +29,28 @@ def read_data(data_path):
     else:
         exit("File format: %s is unsupported" % file_ext)
     return file_data
+
+def get_plate_parameters (fContents):
+    current_r =0
+    lPlate_Id = None
+    lPlate_date = None
+    while current_r < len(fContents) :
+        if lPlate_Id is None:
+            re_prop = re.search('ID1: ([^,\n]*)',','.join(fContents[current_r]))
+            if re_prop:
+        #       logger.info(re_prop.group(1), re_prop.group(2))
+                lPlate_Id = re_prop.group(1)
+        if lPlate_date is None:
+            re_prop = re.search('ID3: ([^\n]*)',','.join(fContents[current_r]))
+            if re_prop:
+        #       logger.info(re_prop.group(1), re_prop.group(2))
+                dt = datetime.strptime(re_prop.group(1), "%d/%m/%Y,%H:%M:%S")
+                lPlate_date = dt.strftime("%d %b %Y %H:%M:%S")
+        current_r += 1
+        if lPlate_Id is not None and lPlate_date is not None:
+            break
+    logger.info(lPlate_date )
+    return lPlate_Id, lPlate_date
 
 # 1-indexed column in which the data block starts.
 data_start_column = 2
@@ -43,14 +66,16 @@ plate_name = os.path.splitext(os.path.basename(orig_file))[0]
 re_name = re.search('plate \d{1,2}', plate_name, re.I)
 if re_name:
     plate_name = re_name.group(0)
-
+plate_id, plate_date = get_plate_parameters(f)
 
 max_row = len(f)
 properties = results.getExperimentProperties()
 if 'block identifier' in properties:
     start_regex = properties.get('block identifier').getPropertyValue()
 else:
-    start_regex = 'Raw Data'
+    start_regex = 'Raw Data[^,]*'
+
+plate_id, plate_date = get_plate_parameters(f)
 
 plate_n = 0
 current_row = 0
@@ -70,7 +95,7 @@ if plate_n > 0:
     results.add()
 
 myplate = results.get(plate_n)
-myplate.addProperty('block identifier', regex_match[0])
+myplate.addProperty('Raw Data Block', regex_match[0])
 myarray = myplate.getResults()
 myTSarray = []
 # Add data to array.
@@ -91,5 +116,7 @@ myplate.addProperty('Plate_Median', str(plateMedian))
 ts_result_AD = [abs(x - plateMedian) for x in myTSarray]
 plateMAD = Calc_median(ts_result_AD)*1.4826
 myplate.addProperty('Plate_MAD', str(plateMAD))
+myplate.addProperty('Read Time', plate_date)
 myplate.setName(plate_name)
+myplate.setBarcode(plate_id)
 plate_n += 1
